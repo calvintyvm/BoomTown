53ffd124044dc87d03400eb35c5f5c414f395916
Calvin Ting <calvinting@protonmail.com>
2018-04-13 00:11:53 -0700

    added masonry and gravatar in itemcard

--- components/ItemCard/ItemCard.js
+++ components/ItemCard/ItemCard.js
@@ -11,9 +11,18 @@ import FlatButton from "material-ui/FlatButton";
 import RaisedButton from "material-ui/RaisedButton";
 import "./styles.css";
 import PropTypes from "prop-types";
+import Masonry from "react-masonry-component";
+import Gravatar from "react-gravatar";
 
 const style = {
   margin: 12
+};
+
+const styleAvatar = {
+  borderRadius: 30
+};
+var masonryOptions = {
+  transitionDuration: 0
 };
 
 const ItemCard = props => {
@@ -21,19 +30,34 @@ const ItemCard = props => {
   //   console.log(itemCards);
   return (
     <div>
-      <Card>
-        <CardMedia>
-          <img src={itemCards.imageurl} />
-        </CardMedia>
-        <CardTitle title="Red Academy" subtitle={itemCards.tags} />
-        <CardText>
-          {/* {itemCards.description.map((item, index) => {
+      <Masonry
+        className={"my-gallery-class"}
+        elementType={"ul"}
+        options={masonryOptions}
+        disableImagesLoaded={false}
+        updateOnEachImageLoad={false}
+      >
+        <Card>
+          <CardMedia>
+            <img src={itemCards.imageurl} />
+          </CardMedia>
+          <CardHeader
+            title="URL Avatar"
+            subtitle="Subtitle"
+            avatar={
+              <Gravatar email="mathews.kyle@gmail.com" style={styleAvatar} />
+            }
+          />
+          <CardTitle title="Red Academy" subtitle={itemCards.tags} />
+          <CardText>
+            {/* {itemCards.description.map((item, index) => {
             return <span>{(index ? ", " : "") + item}</span>;
           })} */}
-          {itemCards.description}
-        </CardText>
-        <RaisedButton label="Borrow" style={style} />
-      </Card>
+            {itemCards.description}
+          </CardText>
+          <RaisedButton label="Borrow" style={style} />
+        </Card>
+      </Masonry>
     </div>
   );
 };
